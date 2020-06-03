8c9d25e1cbe276e855e10b95f221ad175483f9ec
sayali0512 <63386141+sayali0512@users.noreply.github.com>
2020-06-03 12:39:59 +0530

    Update wordpressimporter.sh

--- wordpressimporter.sh
+++ wordpressimporter.sh
@@ -2,12 +2,12 @@
 
 
 downloadwordpressimporter(){
-  wget -p https://wordpress.org/plugins/search/wordpress-importer.0.7.zip/ /home/azureadmin/
+  wget -p https://wordpress.org/plugins/search/wordpress-importer.0.7.zip /home/azureadmin
 }
 
 extractfile(){
   sudo apt install unzip
-  sudo unzip /home/azureadmin/downloads.wordpress.org/plugin/wordpress-importer.0.7.zip
+  sudo unzip /home/azureadmin/wordpress.org/plugins/search/wordpress-importer.0.7.zip
   sudo cp -rf /home/azureadmin/wordpress-importer /var/www/html/wordpress/wp-content/plugins/
   sudo rm -rf home/azureadmin/wordpress-importer
 }
