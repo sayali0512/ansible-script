7784d4295b42a2dfa6a7a1ea0362f5491a3120a0
sayali0512 <63386141+sayali0512@users.noreply.github.com>
2020-06-23 13:26:54 +0530

    Update sql_dump.sh

--- sql_dump.sh
+++ sql_dump.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wp_db_server_name_tar=mysql-yw23wf.mysql.database.azure.com
-wp_db_user_tar=dbadmin@mysql-yw23wf
+wp_db_server_name_tar=mysql-u4nerw.mysql.database.azure.com
+wp_db_user_tar=dbadmin@mysql-u4nerw
 wp_db_password_tar=iTalent@27
 wp_db_name_tar=wordpress
 user_name=azureadmin
@@ -19,11 +19,11 @@ echo db name entered by user: $database
 
 if [ $database = "$dbname" ]; then
   echo database is already exist.. now it will take the existing db backup and imports to target
-  mysqldump -h $wp_db_server_name_tar -u $wp_db_user_tar -p$wp_db_password_tar $wp_db_name_tar > /home/$user_name/db_target_target.sql
-  mysql -h $wp_db_server_name_tar -u $wp_db_user_tar -p$wp_db_password_tar $dbname < /home/$user_name/database.sql
+  mysqldump -h $wp_db_server_name_tar -u $wp_db_user_tar -p$wp_db_password_tar $wp_db_name_tar > /home/$user_name/storage/db_target_target.sql
+  mysql -h $wp_db_server_name_tar -u $wp_db_user_tar -p$wp_db_password_tar $dbname < /home/$user_name/storage/database.sql
 else
   echo database does not exist.. now it will create a new db and imports to target
   mysql -h $wp_db_server_name_tar -u $wp_db_user_tar -p$wp_db_password_tar -e "CREATE DATABASE $database CHARACTER SET utf8;"
-  mysql -h $wp_db_server_name_tar -u $wp_db_user_tar -p$wp_db_password_tar $database < /home/$user_name/database.sql
+  mysql -h $wp_db_server_name_tar -u $wp_db_user_tar -p$wp_db_password_tar $database < /home/$user_name/storage/database.sql
 fi
   mysql -h $wp_db_server_name_tar -u $wp_db_user_tar -p$wp_db_password_tar -e "show databases;"
