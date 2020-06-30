fb2718a2cd56a6f3bb8475c79a405a4e03220fdc
sayali0512 <63386141+sayali0512@users.noreply.github.com>
2020-06-30 11:47:33 +0530

    Update updateconfig.sh

--- updateconfig.sh
+++ updateconfig.sh
@@ -6,10 +6,10 @@
 config_path=/home/azureadmin/storage/site/${5}/wp-config.php
 
 update_target_info() {
-  sudo sed -i "s~{5}~${1}~" ${config_path}
-  sudo sed -i "s~{6}~${2}~" ${config_path}
-  sudo sed -i "s~'DB_PASSWORD', '{7}'~'DB_PASSWORD', '${3}'~" ${config_path}
-  sudo sed -i "s~define('DB_NAME', '{8}');~define('DB_NAME', '${4}');~" ${config_path}
+  sudo sed -i "s~${5}~${1}~" ${config_path}
+  sudo sed -i "s~${6}~${2}~" ${config_path}
+  sudo sed -i "s~'DB_PASSWORD', '${7}'~'DB_PASSWORD', '${3}'~" ${config_path}
+  sudo sed -i "s~define('DB_NAME', '${8}');~define('DB_NAME', '${4}');~" ${config_path}
 }
 
 update_target_info ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8}
