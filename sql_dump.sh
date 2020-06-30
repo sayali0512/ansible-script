187aab8256e241d69f20bc4256891b4036ca7e11
sayali0512 <63386141+sayali0512@users.noreply.github.com>
2020-06-30 15:57:48 +0530

    Update sql_dump.sh

--- sql_dump.sh
+++ sql_dump.sh
@@ -12,7 +12,7 @@ target_db_name=${6}
 
 database=${7}
 
-dbname=$(mysql -h $wp_db_server_name_tar -u $wp_db_user_tar -p$wp_db_password_tar -e "show databases;"| grep $database)
+dbname=$(mysql -h $target_db_server_name -u $target_db_login_name -p$target_db_pass -e "show databases;"| grep $database)
 
 echo db fetched from server: $dbname
 echo db name entered by user: $database
@@ -20,13 +20,13 @@ echo db name entered by user: $database
 
 if [ $database = "$dbname" ]; then
   echo database is already exist.. now it will take the existing db backup and imports to target
-  mysqldump -h $wp_db_server_name_tar -u $wp_db_user_tar -p$wp_db_password_tar $wp_db_name_tar > /home/$user_name/storage/db_target_target.sql
-  mysql -h $wp_db_server_name_tar -u $wp_db_user_tar -p$wp_db_password_tar $dbname < /home/$user_name/storage/database.sql
+  mysqldump -h $target_db_server_name -u $target_db_login_name -p$target_db_pass $target_db_name > /home/$user_name/storage/db_target_target.sql
+  mysql -h $target_db_server_name -u $target_db_login_name -p$target_db_pass $dbname < /home/$user_name/storage/database.sql
 else
   echo database does not exist.. now it will create a new db and imports to target
-  mysql -h $wp_db_server_name_tar -u $wp_db_user_tar -p$wp_db_password_tar -e "CREATE DATABASE $database CHARACTER SET utf8;"
-  mysql -h $wp_db_server_name_tar -u $wp_db_user_tar -p$wp_db_password_tar $database < /home/$user_name/storage/database.sql
+  mysql -h $target_db_server_name -u $target_db_login_name -p$target_db_pass -e "CREATE DATABASE $database CHARACTER SET utf8;"
+  mysql -h $target_db_server_name -u $target_db_login_name -p$target_db_pass $database < /home/$user_name/storage/database.sql
 fi
 
 # Below lines will change the redirection of the URL form source to target
-mysql -h $wp_db_server_name_tar -u $wp_db_user_tar -p$wp_db_password_tar -e "use $database;SET SQL_SAFE_UPDATES=0;UPDATE wp_options SET option_value = replace(option_value, 'http://$source_dns_name', 'https://$target_dns_name') WHERE option_name = 'home' OR option_name = 'siteurl';UPDATE wp_posts SET guid = replace(guid, 'http://$source_dns_name','https://$target_dns_name');UPDATE wp_posts SET post_content = replace(post_content, 'http://$source_dns_name', 'https://$target_dns_name');"
+mysql -h $target_db_server_name -u $target_db_login_name -p$target_db_pass -e "use $database;SET SQL_SAFE_UPDATES=0;UPDATE wp_options SET option_value = replace(option_value, 'http://$source_dns_name', 'https://$target_dns_name') WHERE option_name = 'home' OR option_name = 'siteurl';UPDATE wp_posts SET guid = replace(guid, 'http://$source_dns_name','https://$target_dns_name');UPDATE wp_posts SET post_content = replace(post_content, 'http://$source_dns_name', 'https://$target_dns_name');"
